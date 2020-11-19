a2a968ed76997ae21b4a620ab2e033c01d5cd3e0
JordanW1313 <jwriker@gmail.com>
2020-11-19 16:09:55 -0500

    Validated work ticket number

--- src/ca/durhmacollege/Main.java
+++ src/ca/durhmacollege/Main.java
@@ -1,14 +1,64 @@
 package ca.durhmacollege;
 
 import java.time.LocalDate;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
+        // DEFINED SCANNER AND DEFAULT WORKTICKET OBJECT
+
+        Scanner keyboard = new Scanner(System.in);
+
         WorkTicket defaultTest = new WorkTicket();
 
+        // DEFINED INPUT VARIABLES
+
+        int ticketNumber;
+        String clientID;
+        LocalDate date;
+        String description;
+
+        // INPUT
+
+        System.out.print("Enter the work ticket number: ");
+
+        boolean isValidInput = false;
+
+        while(!isValidInput)
+        {
+            try
+            {
+                ticketNumber = keyboard.nextInt();
+
+                isValidInput = true;
+
+                if(ticketNumber <= 0)
+                {
+                    throw new IllegalArgumentException();
+                }
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Invalid input. Whole number needed. Please try again.");
+                System.out.print("Enter the work ticket number: ");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+            catch(IllegalArgumentException i)
+            {
+                System.out.println("Invalid input. Work ticket number must be non-negative whole number.");
+                System.out.print("Enter the work ticket number: ");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+        }
+
+        System.out.println();
+        System.out.println(defaultTest.toString());
 
     }
 }
