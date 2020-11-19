e9906fe0548679133c75bfd60a15238727dd2ab7
JordanW1313 <jwriker@gmail.com>
2020-11-19 15:43:11 -0500

    Added toString

--- src/ca/durhmacollege/Main.java
+++ src/ca/durhmacollege/Main.java
@@ -8,11 +8,7 @@ public class Main
     public static void main(String[] args)
     {
         WorkTicket defaultTest = new WorkTicket();
-        WorkTicket test = new WorkTicket(1, "A0001", LocalDate.of(2020, 1, 13), "Test Description");
 
-        System.out.print(defaultTest.toString());
-        System.out.println();
-        System.out.println();
-        System.out.print(test.toString());
+
     }
 }
